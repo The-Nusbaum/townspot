85d9007103762391331c5a905da803e9efb3d6c1
mike.hughes <mike.hughes@townspot.tv>
2014-10-06 22:29:25 -0400

    Move database settings to local.dist.php

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -1,11 +1,5 @@
 <?php
-$db = array(
-		'host'     => 'localhost',
-		'port'     => '3306',
-		'user'     => 'root',
-		'password' => 'root',
-		'dbname'   => 'tsz',
-);
+$db = include('local.dist.php');
 
 return array(
     'db' => array_merge(array( 'driver'    => 'PdoMysql'),$db),
