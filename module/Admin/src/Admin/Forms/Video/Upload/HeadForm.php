578ff1c3d120100ef6680ceea673e6d4855d77f6
Ian Nusbaum <ian.nusbaum@townspot.tv>
2015-01-07 20:48:05 -0500

    debug

--- module/Admin/src/Admin/Forms/Video/Upload/HeadForm.php
+++ module/Admin/src/Admin/Forms/Video/Upload/HeadForm.php
@@ -24,12 +24,11 @@ class HeadForm extends Form {
         foreach($colCountries as $c) {
             $countries[$c->getId()] = $c->getName();
         }
-echo '<pre>';
+        
         foreach($colProvinces as $p) {
             var_dump($p->getName());
             $provinces[$p->getId()] = $p->getName();
         }
-die;
 		$cities = array();
 
         foreach($colCities as $c) {
