7ebe31ab492bea36dcfb2a1975398da5f74ffb11
Ian Nusbaum <ian@townspot.tv>
2017-02-26 15:27:21 -0500

    twitch

---
+++ module/Api/src/Api/Controller/PlaylistController.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Api\Controller;
+
+use Zend\EventManager\EventManagerInterface;
+use Zend\View\Model\JsonModel;
+
+class PlaylistController extends \Townspot\Controller\BaseRestfulController
+{
+    public function setEventManager(EventManagerInterface $eventManager)
+    {
+        parent::setEventManager($eventManager);
+
+        $controller = $this;
+
+        $eventManager->attach('dispatch', function ($e) use ($controller) {
+            $this->setModel('Playlist');
+            $this->setMapper(new \Townspot\Playlist\Mapper($this->getServiceLocator()));
+            $this->setEntity(new \Townspot\Playlist\Entity());
+            $this->setResponse(new \Townspot\Rest\Response());
+        }, 100);
+    }
+
+    protected function _playlist(\Townspot\Playlist\Entity $raw) {
+        $media = array();
+        foreach($raw->getMedia() as $m) {
+            $media[] = $m->toArray();
+        }
+        $data[] = array(
+            'id' => $raw->getId(),
+            'name' => $raw->getName(),
+            'desc' => $raw->getDescription(),
+            'media' => $media
+        );
+
+        return $data;
+    }
+
+    public function getAction() {
+        $id = $this->params()->fromRoute('id');
+
+        $playListMapper = new \Townspot\Playlist\Mapper($this->getServiceLocator());
+        $playlist = $playListMapper->find($id);
+
+        $data = $this->_playlist($playlist);
+
+        $this->getResponse()
+            ->setCode(200)
+            ->setSuccess(true)
+            ->setData($data)
+            ->setCount(count($data));
+
+        return new JsonModel($this->getResponse()->build());
+    }
+
+    public function userAction() {
+        $id = $this->params()->fromRoute('id');
+
+        $userMapper = new \Townspot\User\Mapper($this->getServiceLocator());
+        $user = $userMapper->find($id);
+
+        $playlists = $user->getPlaylists();
+
+        $data = array();
+
+        foreach($playlists as $p) {
+            $data[] = $this->_playlist($p);
+        }
+
+        $this->getResponse()
+            ->setCode(200)
+            ->setSuccess(true)
+            ->setData($data)
+            ->setCount(count($data));
+
+        return new JsonModel($this->getResponse()->build());
+    }
+
+    public function listAction() {
+        $id = $this->params()->fromRoute('id');
+
+        $playListMapper = new \Townspot\Playlist\Mapper($this->getServiceLocator());
+        $playlists = $playListMapper->findAll();
+
+        $data = array();
+
+        foreach($playlists as $p) {
+            $data[] = $this->_playlist($p);
+        }
+
+        $this->getResponse()
+            ->setCode(200)
+            ->setSuccess(true)
+            ->setData($data)
+            ->setCount(count($data));
+
+        return new JsonModel($this->getResponse()->build());
+    }
+
+    public function addAction() {
+        $pid = $this->params()->fromRoute('pid');
+        $mid = $this->params()->fromRoute('mid');
+
+        $playListMapper = new \Townspot\Playlist\Mapper($this->getServiceLocator());
+        $playlist = $playListMapper->find($pid);
+
+        $mediaMapper = new \Townspot\Media\Mapper($this->getServiceLocator());
+        $media = $mediaMapper->find($mid);
+
+        $playlist->addMedia($media);
+
+        $data = $playlist->asArray();
+
+        $this->getResponse()
+            ->setCode(200)
+            ->setSuccess(true)
+            ->setData($data)
+            ->setCount(count($data));
+
+        return new JsonModel($this->getResponse()->build());
+    }
+
+    public function removeAction() {
+        $pid = $this->params()->fromRoute('pid');
+        $mid = $this->params()->fromRoute('mid');
+
+        $playListMapper = new \Townspot\Playlist\Mapper($this->getServiceLocator());
+        $playlist = $playListMapper->find($pid);
+
+        $mediaMapper = new \Townspot\Media\Mapper($this->getServiceLocator());
+        $media = $mediaMapper->find($mid);
+
+        $playlist->removeMedia($media);
+
+        $data = $playlist->asArray();
+
+        $this->getResponse()
+            ->setCode(200)
+            ->setSuccess(true)
+            ->setData($data)
+            ->setCount(count($data));
+
+        return new JsonModel($this->getResponse()->build());
+    }
+
+    public function createAction() {
+        $uid = $this->params()->fromPost('uid');
+        $name = $this->params()->fromPost('name');
+        $desc = $this->params()->fromPost('desc');
+
+        $playListMapper = new \Townspot\Playlist\Mapper($this->getServiceLocator());
+        $userMapper = new \Townspot\User\Mapper($this->getServiceLocator());
+
+        $success = true;
+        $code = 200;
+
+        $user = $userMapper->find($uid);
+        if($user instanceof \Townspot\User\Entity) {
+            $playlist = new \Townspot\Playlist\Entity();
+            $playlist->setUser($user);
+            $playlist->setName($name);
+            $playlist->setDesc($desc);
+        } else {
+            $success = false;
+            $data = "No such user";
+            $code = 404;
+        }
+
+        $playListMapper->save($playlist);
+
+        $data = $this->_playlist($playlist);
+
+        $this->getResponse()
+            ->setCode($code)
+            ->setSuccess($success)
+            ->setData($data)
+            ->setCount(count($data));
+
+        return new JsonModel($this->getResponse()->build());
+    }
+
+    public function deleteAction() {
+        $pid = $this->params()->fromRoute('pid');
+
+        $playListMapper = new \Townspot\Playlist\Mapper($this->getServiceLocator());
+        $playlist = $playListMapper->find($pid);
+
+        $playListMapper->delete($playlist);
+
+        $data = null;
+
+        $this->getResponse()
+            ->setCode(200)
+            ->setSuccess(true)
+            ->setData($data)
+            ->setCount(count($data));
+
+        return new JsonModel($this->getResponse()->build());
+    }
+} 
