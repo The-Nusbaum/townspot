a3bca75a9b220d2bd848f72fa96a7da2ae42d650
Ian Nusbaum <ian@townspot.tv>
2017-02-27 19:34:36 -0500

    playlists

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -51,5 +51,22 @@ var playlist = {
                 }
             );
         });
+    },
+    populateProfile: function() {
+        $('#playlists').each(function(){
+            var uid = $(this).attr('data-uid');
+            $.get('/api/playlist/user/' + uid,
+                function(response){
+                    if(response.success) {
+                        var data = response.data;
+                        $(data).each(function(){
+                            var p = this[0];
+                            var html = "<option value='" + p.id + "'>" + p.name + "</option>";
+                            $('#playlists option:last').before(html);
+                        });
+                    }
+                }
+            );
+        });
     }
 };
