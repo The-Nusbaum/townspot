3e8385b6752b1ebfcfd71bad33f36301155651a9
Ian Nusbaum <ian@townspot.tv>
2017-03-01 22:32:41 -0500

    playlists

--- module/Api/src/Api/Controller/PlaylistController.php
+++ module/Api/src/Api/Controller/PlaylistController.php
@@ -136,7 +136,7 @@ class PlaylistController extends \Townspot\Controller\BaseRestfulController
 
         $playListMapper = new \Townspot\Playlist\Mapper($this->getServiceLocator());
         $playlist = $playListMapper->find($pid);
-
+var_dump(get_class_methods($playListMapper));die;
         //$playlist->removeMedia($mid);
         $playListMapper->getConnection()->execute("delete from playlist_media where playlist_id = $pid and media_id = $mid");
 
